95f0c458eb0b5ab1df35bea29338e6569d104dc9
Jose Santos <josejaviersantossantana@gmail.com>
2025-07-15 20:37:35 -0400

    elimar codigo innecesario en app.js

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 const {crearDatos} = require('./controllers/create.controller')
-crearDatos();
-=======
 const { listarUsuarios } = require('./controllers/lista.controller')
 listarUsuarios()
->>>>>>> listar-datos
