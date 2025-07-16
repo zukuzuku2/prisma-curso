db002d211a1529602e89488f169f40e08091071a
Jose Santos <josejaviersantossantana@gmail.com>
2025-07-15 20:29:08 -0400

    aniadir controller listar datos

--- app.js
+++ app.js
@@ -1,23 +1,2 @@
-const { PrismaClient } = require("./generated/prisma");
-const prisma = new PrismaClient(); // --> conexion a prisma
-
-async function crearDatos() {
-  const newUser = await prisma.User.create({
-    data: {
-      name: "Maytee",
-      email: "maytee@gmail.com",
-    },
-  });
-  console.log(newUser);
-}
-
-//crearDatos()
-
-	
-	
-async function listarUsuarios() {
-  const listUser = await prisma.User.findMany() //--> devuelve un arreglo de todos los usuarios
-	console.log(listUser);
-}
-
+const { listarUsuarios } = require('./controllers/lista.controller')
 listarUsuarios()
